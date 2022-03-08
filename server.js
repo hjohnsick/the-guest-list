d598fa39674c7972615baf3c8ee2acc621f09e30
hjohnsick <hjohnsick@yahoo.com>
2022-03-07 21:37:11 -0500

    added starting code for server.js

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+const path = require("path");
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({});
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+require("dotenv").config();
+
+const sess = {
+  secret: process.env.SESSION,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(session(sess));
+// turn on routes
+app.use(routes);
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now listening"));
+});
