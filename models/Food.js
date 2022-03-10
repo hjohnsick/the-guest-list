87dcdf9efa4f943a9a63832eb53b23984dbe70b9
hjohnsick <hjohnsick@yahoo.com>
2022-03-09 23:14:48 -0500

    added routes for food

--- models/Food.js
+++ models/Food.js
@@ -1,40 +1,39 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
 
-
 class Food extends Model {}
 //foodtable
 Food.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement:true,
-        },
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
 
-        food: {
-            type: DataTypes.STRING,
-            allowNull: true,
-        },
+    food: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
 
-        guest_id:{
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            references: {
-                model: "guestList",
-                key: "id",
-            }
-        }
-    },  
+    guest_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: "guestList",
+        key: "id",
+      },
+    },
+  },
 
-    {
-        sequelize,
-        timestamps: false,
-        freezeTableName: true,
-        underscored: true,
-        modelName: "guestList",
-    }  
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "guestList",
+  }
 );
 
 module.exports = Food;
