7ba78e455d86f6534adba28e755180ed584d71c1
jackiem2 <jacqueline.merlan@gmail.com>
2022-03-09 18:37:04 -0600

    guest routes

---
+++ controllers/api/guestRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const config = require('../config/connection');
+
+// Get all guests
+router.get('/')
+
+
+module.exports = router;
