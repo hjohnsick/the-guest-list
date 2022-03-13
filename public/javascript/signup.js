682f94689e28a1e9b7ce1397ea77b349ab5ac945
hjohnsick <hjohnsick@yahoo.com>
2022-03-12 19:23:15 -0500

    added ability to signup user via front end and redirect to guestlist

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -1,17 +1,17 @@
 async function signupFormHandler(event) {
   event.preventDefault();
 
-  const firstName = document.querySelector("#firstname-signup").value.trim();
-  const lastName = document.querySelector("#lastname-signup").value.trim();
+  const first_name = document.querySelector("#firstname-signup").value.trim();
+  const last_name = document.querySelector("#lastname-signup").value.trim();
   const email = document.querySelector("#email-signup").value.trim();
   const password = document.querySelector("#password-signup").value.trim();
 
-  if (firstName && lastName && email && password) {
+  if (first_name && last_name && email && password) {
     const response = await fetch("/api/users", {
       method: "post",
       body: JSON.stringify({
-        firstName,
-        lastName,
+        first_name,
+        last_name,
         email,
         password,
       }),
