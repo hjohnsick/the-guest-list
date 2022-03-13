f15a273ccccb24d269d2a7991d7054b29c580a5a
hjohnsick <hjohnsick@yahoo.com>
2022-03-13 09:15:47 -0400

    able to open and close login modal

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,17 +1,41 @@
-// modal
+// signup modal
 const signupButton = document.querySelector("#signup");
-const modalBg = document.querySelector(".modal-background");
-const modal = document.querySelector(".modal");
-const modalClose = document.querySelector("#modal-close");
+const modalSignUp = document.querySelector("#modal-sign-up");
+const modalBgSignUp = document.querySelector("#modal-background-signup");
+const modalCloseSignup = document.querySelector("#modal-close-signup");
 
+// display signup modal when signup button is clicked
 signupButton.addEventListener("click", () => {
-  modal.classList.add("is-active");
+  modalSignUp.classList.add("is-active");
 });
 
-modalBg.addEventListener("click", () => {
-  modal.classList.remove("is-active");
+// close signup modal when background is clicked
+modalBgSignUp.addEventListener("click", () => {
+  modalSignUp.classList.remove("is-active");
 });
 
-modalClose.addEventListener("click", () => {
-  modal.classList.remove("is-active");
+// close signup modal when x is clicked
+modalCloseSignup.addEventListener("click", () => {
+  modalSignUp.classList.remove("is-active");
+});
+
+// login modal
+const loginButton = document.querySelector("#login");
+const modalLogin = document.querySelector("#modal-login");
+const modalBgLogin = document.querySelector("#modal-background-login");
+const modalCloseLogin = document.querySelector("#modal-close-login");
+
+// display login modal when login button is clicked
+loginButton.addEventListener("click", () => {
+  modalLogin.classList.add("is-active");
+});
+
+// close login modal when background is clicked
+modalBgLogin.addEventListener("click", () => {
+  modalLogin.classList.remove("is-active");
+});
+
+// close login modal when x is clicked
+modalCloseLogin.addEventListener("click", () => {
+  modalLogin.classList.remove("is-active");
 });
