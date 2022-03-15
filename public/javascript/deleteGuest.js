fe0663a8e548927c6ecc7bfe160f425edbf6802a
hjohnsick <hjohnsick@yahoo.com>
2022-03-14 21:57:10 -0400

    added ability to delete a guest

--- public/javascript/deleteGuest.js
+++ public/javascript/deleteGuest.js
@@ -1,3 +1,15 @@
 async function deleteGuestHandler(event) {
   event.preventDefault();
+
+  const guestId = event.target.id;
+
+  const response = await fetch(`/api/guestlist/${guestId}`, {
+    method: "DELETE",
+  });
+
+  if (response.ok) {
+    document.location.replace("/theguestlist");
+  } else {
+    alert(response.statusText);
+  }
 }
