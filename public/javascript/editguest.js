5398041317f14d3b1f80ce7f3b8ed210cb0d9f35
Nate Farver <farvercreek@gmail.com>
2022-03-16 14:25:19 -0400

    code rework for edit feature

--- public/javascript/editguest.js
+++ public/javascript/editguest.js
@@ -1,65 +1,82 @@
+//Get id of the clicked button
+function editGuest() {
+  //   id = guestId.getAttribute("data-guestId");
+  //   const editIcon = document.querySelector(`#guest-${id}`);
+  //   console.log(id);
+  //   console.log(editIcon);
+  const modalEditGuest = document.querySelector("#modal-editGuest");
+  modalEditGuest.classList.add("is-active");
+  console.log("buttonClicked");
+  const modalBgEditGuest = document.querySelector(
+    "#modal-background-editGuest"
+  );
+  const modalCloseEditGuest = document.querySelector("#modal-close-editGuest");
+  // close editGuest modal when editGuest background is clicked
+  modalBgEditGuest.addEventListener("click", () => {
+    modalEditGuest.classList.remove("is-active");
+  });
+  // close editGuest modal when x is clicked
+  modalCloseEditGuest.addEventListener("click", () => {
+    modalEditGuest.classList.remove("is-active");
+  });
+}
+
+//Get id of the clicked button
+function getId(guestId) {
+  console.log(guestId);
+  id = guestId.getAttribute("data-guestId");
+  const editIcon = document.querySelector(`#guest-${id}`);
+  return editIcon;
+}
+
 async function editGuestFormHandler(event) {
   event.preventDefault();
-    console.log("clicked")
-    const first_name = document.querySelector("#firstname-editGuest").value.trim();
-    const last_name = document.querySelector("#lastname-editGuest").value.trim();
-    const street = document.querySelector("#address-editGuest").value.trim();
-    const city = document.querySelector("#city-editGuest").value.trim();
-    const state = document.querySelector("#state-editGuest").value.trim();
-    const zipcode = document.querySelector("#zipcode-editGuest").value.trim();
-    const email = document.querySelector("#email-editGuest").value.trim();
-    const phone_number = document.querySelector("#phonenumber-editGuest").value.trim();
-    const rsvp = document.querySelector("#rsvp-editGuest:checked").value.trim();
-    const food_id = document.querySelector("#foodchoice-editGuest").value.trim();
+  console.log("Submit button was clicked");
 
-    const editButton = document.querySelector("editGuestFormHandler").value.trim();
-    const modalEditGuest = document.querySelector(".modal-editGuest").value.trim();
-    const modalBgEditGuest = document.querySelector("#modal-background-editGuest");
-    const modalCloseEditGuest = document.querySelector("#modal-close-editGuest");
-   
-   
+  const first_name = document
+    .querySelector("#firstname-editGuest")
+    .value.trim();
+  const last_name = document.querySelector("#lastname-editGuest").value.trim();
+  const street = document.querySelector("#address-editGuest").value.trim();
+  const city = document.querySelector("#city-editGuest").value.trim();
+  const state = document.querySelector("#state-editGuest").value.trim();
+  const zipcode = document.querySelector("#zipcode-editGuest").value.trim();
+  const email = document.querySelector("#email-editGuest").value.trim();
+  const phone_number = document
+    .querySelector("#phonenumber-editGuest")
+    .value.trim();
+  //   const rsvp = document.querySelector("#rsvp-editGuest:checked").value;
+  const rsvp = document.querySelector(
+    'input[name="Has the guest RSVPd?"]:checked')?.value;
+  const food_id = document.querySelector("#foodchoice-editGuest").value;
+  //   const clickedId = event.target.id;
+  //   const guestId = clickedId.split("-").pop();
+  const guestId = getId(event.target.id);
+  //   const id = 1;
+  const response = await fetch(`/api/guestList/${guestId}`, {
+    method: "PUT",
+    body: JSON.stringify({
+      first_name,
+      last_name,
+      street,
+      city,
+      state,
+      zipcode,
+      email,
+      phone_number,
+      rsvp,
+      food_id,
+    }),
+    headers: { "Content-Type": "application/json" },
+  });
 
-    // display editGuest modal when editGuest button is clicked
-    editButton.addEventListener("click", () => {
-      modalEditGuest.classList.add("is-active");
-    });
-    // close editGuest modal when editGuest background is clicked
-    modalBgEditGuest.addEventListener("click", () => {
-      modalEditGuest.classList.remove("is-active");
-    });
-    // close editGuest modal when x is clicked
-    modalCloseEditGuest.addEventListener("click", () => {
-      modalEditGuest.classList.remove("is-active");
-    });
-
-    console.log(id)
-
-    if (first_name && last_name && street && city && state && zipcode && email && phone_number && rsvp) {
-      const response = await fetch("/api/guestlist/", {
-        method: "put",
-        body: JSON.stringify({
-          first_name,
-          last_name,
-          street,
-          city,
-          state,
-          zipcode,
-          email,
-          phone_number,
-          rsvp,
-          food_id
-        }),
-        headers: { "Content-Type": "application/json" },
-      });
-  
-      if (response.ok) {
-        document.location.replace("/theguestlist");
-      } else {
-        alert(response.statusText);
-      }
-    }
+  if (response.ok) {
+    document.location.replace("/theguestlist");
+  } else {
+    alert(response.statusText);
   }
-  
-  document
-    .querySelector("#editGuest-form")
-    .addEventListener("submit", editGuestFormHandler);
+};
+
+document
+  .querySelector("#editGuest-form")
+  .addEventListener("submit", editGuestFormHandler);
