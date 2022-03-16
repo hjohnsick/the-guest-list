7b4a3bcc38e5dd9423f5b722313e04fa94a48108
Nate Farver <farvercreek@gmail.com>
2022-03-15 21:48:09 -0400

    this is the end code I have edit feature not working

--- public/javascript/editguest.js
+++ public/javascript/editguest.js
@@ -1,5 +1,5 @@
 async function editGuestFormHandler(event) {
-    event.preventDefault();
+  event.preventDefault();
     console.log("clicked")
     const first_name = document.querySelector("#firstname-editGuest").value.trim();
     const last_name = document.querySelector("#lastname-editGuest").value.trim();
@@ -11,13 +11,31 @@ async function editGuestFormHandler(event) {
     const phone_number = document.querySelector("#phonenumber-editGuest").value.trim();
     const rsvp = document.querySelector("#rsvp-editGuest:checked").value.trim();
     const food_id = document.querySelector("#foodchoice-editGuest").value.trim();
-    const id = event.target.id;
-    
+
+    const editButton = document.querySelector("editGuestFormHandler").value.trim();
+    const modalEditGuest = document.querySelector(".modal-editGuest").value.trim();
+    const modalBgEditGuest = document.querySelector("#modal-background-editGuest");
+    const modalCloseEditGuest = document.querySelector("#modal-close-editGuest");
    
+   
+
+    // display editGuest modal when editGuest button is clicked
+    editButton.addEventListener("click", () => {
+      modalEditGuest.classList.add("is-active");
+    });
+    // close editGuest modal when editGuest background is clicked
+    modalBgEditGuest.addEventListener("click", () => {
+      modalEditGuest.classList.remove("is-active");
+    });
+    // close editGuest modal when x is clicked
+    modalCloseEditGuest.addEventListener("click", () => {
+      modalEditGuest.classList.remove("is-active");
+    });
+
     console.log(id)
 
     if (first_name && last_name && street && city && state && zipcode && email && phone_number && rsvp) {
-      const response = await fetch(`/api/guestlist/${id}`, {
+      const response = await fetch("/api/guestlist/", {
         method: "put",
         body: JSON.stringify({
           first_name,
